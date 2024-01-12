c2ecc50127a6959638246b9dac869a56d84e981a
codemon39 <jamespk0309@gmail.com>
2024-01-12 08:53:24 -0600

    initial

--- routes/api/router.js
+++ routes/api/router.js
@@ -6,17 +6,18 @@ router.get("/test", (req, res) => res.json(animals.monsters));
 
 router.post("/gameStart", (req, res) => {
   const data = req.body.data;
-  console.log(data);
 
   const monster1 = animals.monsters.find(
-    (monster) => monster.id == data.playerid
+    (monster) => monster.id == data.playerId
   );
+
   const monster2 = animals.monsters.find(
-    (monster) => monster.id == data.computerid
+    (monster) => monster.id == data.computerId
   );
 
   const monster1Sum =
     monster1.defense + monster1.hp + monster1.speed + monster1.attack;
+
   const monster2Sum =
     monster2.defense + monster2.hp + monster2.speed + monster2.attack;
 
@@ -33,6 +34,7 @@ router.post("/gameStart", (req, res) => {
 
 router.post("/result", (req, res) => {
   const id = req.body.id;
+  console.log(id);
   const monster = animals.monsters.find((monster) => monster.id === id);
   res.json(monster);
 });
